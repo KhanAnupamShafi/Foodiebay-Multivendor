76f7b78a7fbfd0d97f2bb90dee387a71f53c0e0c
KhanAnupamShafi <deep71.bd@gmail.com>
2022-10-23 22:38:13 +0600

    styled-component installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import styled from "styled-components";
+import { Amazon } from "styled-icons/fa-brands";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <RootContainer className="App">
+      <h1>
+        Hello I am inside styled component
+        {<RedAmazon size="48" title="Amazon Logo" />}
+      </h1>
+    </RootContainer>
   );
 }
 
 export default App;
+
+const RootContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`;
+
+const RedAmazon = styled(Amazon)`
+  color: red;
+`;
