bb39d7bb1b08110066362939246d4a5b401fb8e1
KhanAnupamShafi <deep71.bd@google.com>
2023-11-24 22:01:38 +0600

    update ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Uber", "sans-serif"],
+        sans: ['Uber', 'sans-serif'],
       },
       colors: {
-        lightOrange: "#FEEFE7",
-        softOrange: "#FBCDB4",
-        textMain: "#333",
-        textLight: "#666",
-        textDark: "#333",
-        textBody: "#333",
-        textContrast: "#eee",
+        lightOrange: '#FEEFE7',
+        softOrange: '#FBCDB4',
+        textMain: '#333',
+        textLight: '#666',
+        textDark: '#333',
+        textBody: '#333',
+        textContrast: '#eeeeee',
       },
-      opacity: ["disabled"],
+      opacity: ['disabled'],
     },
     daisyui: {
       themes: [
         {
           winter: {
-            ...require("daisyui/src/colors/themes")["[data-theme=winter]"],
-            primary: "#e34",
-            "primary-focus": "white",
+            ...require('daisyui/src/colors/themes')[
+              '[data-theme=winter]'
+            ],
+            primary: '#e34',
+            'primary-focus': 'white',
           },
         },
       ],
     },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/forms")],
+  plugins: [require('daisyui'), require('@tailwindcss/forms')],
   corePlugins: {
     preflight: false,
   },
